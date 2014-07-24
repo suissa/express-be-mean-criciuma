fc6913020b17561629d0bf79a715ab3b3277a63d
Suissa <jnascimento@gmail.com>
2014-07-23 22:04:44 -0300

    Adicionado o controller e a view correta para consultar cerveja

--- controllers/beers.js
+++ controllers/beers.js
@@ -20,6 +20,18 @@ module.exports = {
         beers: data
       });
     })
+  },
+  renderBeer: function(req, res){
+    var query = {_id: req.params.id};
+    Beer.findOne(query, function(err, data){
+      if(err){
+        res.render('error', { error: err});
+      }
+      res.render('beers/show', { 
+        title: 'Consulta de cerveja', 
+        beer: data
+      });
+    })
   }
 };
 
